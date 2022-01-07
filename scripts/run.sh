9a3b09cab7f6fb8aba430630c4979360cc2e579a
Oliver-ingle <oliver.ingle@gmail.com>
2022-01-07 15:36:38 +0100

    added dotenv to gitignore, oops...

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,15 +6,15 @@ read opt
 if [ $opt == 'js' ]; then
 printf "> Specify file: "
 read f
-printf "\n\n\t\t----- RUNNING APP ------\n\n"
+printf "\n\n\t\t------ RUNNING APP ------\n\n"
 node $f
 
 elif [ $opt == 'ts' ]; then
-printf  "\n\n\t\t----- RUNNING TYPESCRIPT COMPILER ------\n"
+printf  "\n\n\t\t------ RUNNING TYPESCRIPT COMPILER ------\n"
 tsc
 
 elif [ $opt == 'dev' ]; then
-printf  "\n\n\t\t----- RUNNING NODEMON ------\n"
+printf  "\n\n\t\t------ RUNNING NODEMON ------\n"
 nodemon ./src/index.ts
 
 elif [ $opt == 'help' ]; then
